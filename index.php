56494e3f2b3948e28c70fe8170e177e59f508110
Sainan <63328889+Sainan@users.noreply.github.com>
2025-06-23 02:20:50 +0200

    fix search hash during load

--- index.php
+++ index.php
@@ -31,10 +31,18 @@
 			document.getElementById("results-status").textContent = "Loading...";
 		}
 
-		document.getElementById("query").oninput = function()
-		{
-			document.getElementById("results-status").textContent = "Sorry, data is still downloading. Your query will be processed ASAP.";
-		};
+               document.getElementById("query").oninput = function()
+               {
+                       if (this.value == "")
+                       {
+                               history.replaceState({}, undefined, "/");
+                       }
+                       else
+                       {
+                               history.replaceState({}, undefined, "/#q=" + encodeURIComponent(this.value));
+                       }
+                       document.getElementById("results-status").textContent = "Sorry, data is still downloading. Your query will be processed ASAP.";
+               };
 
 		Promise.all([
 			getDictPromise(),
